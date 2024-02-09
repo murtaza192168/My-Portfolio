37bb02aaf98fa0bc548d99296082cd49aacc2711
murtaza192168 <murtazacloudwork1252@gmail.com>
2024-02-09 21:17:59 +0530

    Add view-resume button

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,14 +2,21 @@ import React from "react"
 import { getImageUrl } from "../../utils"
 import styles from "../Hero/Hero.module.css"
 
+
+
 export const Hero = () => {
     return(
         <section className={styles.container} >
             <div className={styles.content}>
              <h1 className={styles.title}>Hi, I'm Murtaza</h1>
              <p className={styles.description}>Being a <span className={styles.se}>Software Engineer</span>, eager to leverage a strong academic background and a passion for crafting top-notch software solutions.</p>
-             {/* For contact Me Button */}
-             <a className={styles.contactBtn} href="mailto:murtaza.3118033.co@mhssce.ac.in">Contact Me</a>
+             {/* For contact Me and View-Resume Button */}
+            
+            <div className={styles.anchorBtn}>
+            <a className={styles.contactBtn} href="mailto:murtaza.3118033.co@mhssce.ac.in">Contact Me</a>
+             <a className={styles.resumeBtn} href="https://www.canva.com/design/DAF4Plmmpwo/dmDhWjiar5YMfJA_DQOHMA/view?utm_content=DAF4Plmmpwo&utm_campaign=designshare&utm_medium=link&utm_source=editor">View Resume</a>
+            </div>
+
             </div>
             {/* For Image */}
             <img width="400px" className={styles.heroImg} src={getImageUrl("hero/heroImage.png")} alt="hero image of me"/>
